ea044b625e720143e2699f62dc7554960c2aede2
edella2 <edella2@gmail.com>
2015-10-02 16:38:14 -0700

    Add 4.5 pair

--- week-4/leap-years/my_solution.rb
+++ week-4/leap-years/my_solution.rb
@@ -1,6 +1,18 @@
 # Leap Years
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge [by myself, with: Eric Freeburg ].
 
 
 # Your Solution Below
+
+def leap_year?(year)
+  if year % 400 == 0
+    return true
+  elsif year % 100 == 0
+    return false
+  elsif year % 4 == 0
+    return true
+  else
+    return false
+  end
+end
