420ba888b86212ebaada5c2433fb0276e961411e
edella2 <edella2@gmail.com>
2015-10-02 16:33:55 -0700

    Pair 4.5

---
+++ week-4/calculate-grade/my_solution.rb
@@ -0,0 +1,20 @@
+# Calculate a Grade
+
+# I worked on this challenge [by myself, with: ].
+
+
+# Your Solution Below
+
+def get_grade(n)
+  if n >= 90 
+    return "A"
+  elsif n >= 80 
+    return "B"
+  elsif n >= 70
+    return "C"
+  elsif n >= 60
+    return "D"
+  else
+    return "F"
+  end
+end
