bb3a6ec4093cfa36f476c8b22b057caf2ecb28d6
edella2 <edella2@gmail.com>
2015-10-10 13:52:39 -0700

    Add Die class

--- week-5/die-class/my_solution.rb
+++ week-5/die-class/my_solution.rb
@@ -62,9 +62,38 @@ end
 
 # 4. Reflection
 
+
+=begin
 What is an ArgumentError and why would you use one?
+
+An ArgumentError should be raised when arguments input into methods or classes will return something that is not possible.  Having a Die with -10 or 0 sides is not possible.  An argument error alerts the user, that the argument they input will not work.
+
 What new Ruby methods did you implement? What challenges and successes did you have in implementing them?
+
+The newest method I learned was 'raise'.  I have never tried raising errors before and it was a little confusing trying to implement it for this problems solution.  I tried writing "raise "ArgumentError"" initially.  The correct way is to not include it as a string.
+
 What is a Ruby class?
+
+A class is like a blueprint.  String, arrays, hashes are all classes.  They all have their own innate methods.
+
 Why would you use a Ruby class?
+
+You would use a ruby class to create new methods.  Something like creating a class for dogs and methods for them barking is an example of this.  You could initialze a class that takes in a dogs age and a method that makes them bark each time for their age anytime it is called.
+
+(something like)
+baxter = Dog.new(5)
+baxter.bark
+
 What is the difference between a local variable and an instance variable?
+
+local variables are scoped to just the method they are called in.  A instance variable is scoped to the class.
+
+local_variable = "I am a local variable, I am tied to the method in which I exist"
+
+@instance_variable = "I am a instance variable, I am tied to the class or my specific object"
+
 Where can an instance variable be used?
+
+An instance variable can be used inside of the class.  Or anytime it is associated with the object
+
+=end
