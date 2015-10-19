2b9f73d031f2b5a026fb6254cc0c6616ef76fabc
edella2 <edella2@gmail.com>
2015-10-18 19:48:09 -0700

    Add refelction

--- week-6/credit-card/my_solution.rb
+++ week-6/credit-card/my_solution.rb
@@ -104,11 +104,59 @@ end
 
 # Refactored Solution
 
+class CreditCard
+  
+  def initialize(card_num)
+    raise ArgumentError.new("Please input a 16 digit card number") if card_num.to_s.length != 16
+    @card_num = card_num    
+  end
+  
+  def check_card
+    card_array = @card_num.to_s.split("").map(&:to_i)
+    idx = 0 
+      
+    while idx < card_array.length
+      card_array[idx] *= 2      
+      idx += 2
+    end 
+     
+    sum = 0
+    card_array.each do |digit|
+      if digit > 9
+        digit.to_s.split("").each do |num|          
+          sum += num.to_i          
+        end
+      else
+        sum += digit        
+      end      
+    end
+    
+    if sum % 10 == 0
+      return true
+    else
+      return false
+    end
+    
+  end
+end
 
 
 
 
-
-
+=begin
 
 # Reflection
+What was the most difficult part of this challenge for you and your pair?
+
+converting a digit, to a string, placing the strings into an array and then converting the strings into a integer again.  It got a little confusing keeping track of everything
+
+
+What new methods did you find to help you when you refactored?
+
+We found one with using .map(&:to_i).  this was one of my first time "injecting" a method into a enumerable.
+
+What concepts or learnings were you able to solidify in this challenge?
+
+I feel I have an even better handle on using enumerables now after this challenge.
+
+=end
